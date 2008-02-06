786547bcef17f2dbb1376e1778b17e91f369f35a
smuzaffar <cmsbuild@cern.ch>
2008-02-06 10:24:06 +0000

    fixed projectAreaRename.pl script to relocate the project area

--- projectAreaRename.pl
+++ projectAreaRename.pl
@@ -1,29 +1,35 @@
 #!/usr/bin/env perl
 BEGIN{unshift @INC,$ENV{SCRAM_TOOL_HOME};}
 use Cache::CacheUtilities;
+use File::Basename;
 use UNIVERSAL qw(isa);
 
 my $olddir=shift || die "Missing old installation path";
-my $localtop=shift || die "Missing current installation path";
+my $newtop=shift || die "Missing current installation path";
 my $arch=shift || die "Missing SCRAM arch";
-if($olddir ne $localtop)
+my $dir=shift; 
+if(!defined $dir){$dir=`/bin/pwd`; chomp $dir;}
+my $rel=$dir;
+while((!-d "${rel}/.SCRAM") && ($rel!~/^[\.\/]$/)){$rel=dirname($rel);}
+if(!-d "${rel}/.SCRAM"){die "$dir is not a SCRAM-based project area.";}
+
+if($olddir ne $newtop)
 {
-  my $bdir="${localtop}/.SCRAM/${arch}";
   foreach my $file ("ProjectCache.db","DirCache.db","ToolCache.db")
   {
-    my $cache=&Cache::CacheUtilities::read("${bdir}/${file}");
-    if(&processbinary($cache)){&Cache::CacheUtilities::write($cache,"${bdir}/${file}");}
+    my $cache=&Cache::CacheUtilities::read("${rel}/.SCRAM/${arch}/${file}");
+    if(&processbinary($cache)){&Cache::CacheUtilities::write($cache,"${rel}/.SCRAM/${arch}/${file}");}
   }
-  $bdir="${bdir}/MakeData";
-  foreach my $file ("DirCache","Tools","src.mk","variables.mk")
-  {&processtext("${bdir}/${file}");}
+  foreach my $file ("${arch}/MakeData","cache","InstalledTools")
+  {&processtext("${rel}/.SCRAM/${file}",1);}
 }
 
 sub processtext ()
 {
   my $file=shift;
+  my $recursive=shift || 0;
   if(-f $file){&processfile($file);}
-  elsif(-d $file){&processdir($file);}
+  elsif(-d $file){&processdir($file,$recursive);}
 }
 
 sub processfile ()
@@ -36,7 +42,7 @@ sub processfile ()
   while(my $line=<$inref>)
   {
     chomp $line;
-    if($line=~s/$olddir/$localtop/g){$flag=1;}
+    if($line=~s/$olddir/$newtop/g){$flag=1;}
     print $outref "$line\n";
   }
   close($inref);
@@ -54,14 +60,16 @@ sub processfile ()
 sub processdir ()
 {
   my $dir=shift;
+  my $recursive=shift || 0;
   my $dref;
   my $flag=0;
   opendir($dref,$dir) || die "Can not open directory for reading: $dir";
   foreach my $file (readdir($dref))
   {
     if($file=~/^\./){next;}
-    if(-d "${dir}/${file}"){next;}
-    $flag+=&processfile("${dir}/${file}");
+    if(-d "${dir}/${file}")
+    {if($recursive){processdir("${dir}/${file}",$recursive);}}
+    else{$flag+=&processfile("${dir}/${file}");}
   }
   closedir($dref);
   if($flag>0)
@@ -88,7 +96,7 @@ sub processbinary ()
       my $v=$cache->{$k};
       if(isa($v,"HASH")){$changed+=&processbinary(\%$v);}
       elsif(isa($v,"ARRAY")){$changed+=&processbinary(\@$v);}
-      elsif($v=~s/$olddir/$localtop/g){$changed=1;$cache->{$k}=$v;}
+      elsif($v=~s/$olddir/$newtop/g){$changed=1;$cache->{$k}=$v;}
     }
   }
   elsif(isa($cache,"ARRAY"))
@@ -99,7 +107,7 @@ sub processbinary ()
       my $v=$cache->[$i];
       if(isa($v,"HASH")){$changed+=&processbinary(\%$v);}
       elsif(isa($v,"ARRAY")){$changed+=&processbinary(\@$v);}
-      elsif($v=~s/$olddir/$localtop/g){$changed=1;$cache->[$i]=$v;}
+      elsif($v=~s/$olddir/$newtop/g){$changed=1;$cache->[$i]=$v;}
     }
   }
   return $changed;
