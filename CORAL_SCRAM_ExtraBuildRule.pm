d290532a5cfaa4a088f9ed18a5cac79ece6e2a47
cmsbuild <cmsbuild@cern.ch>
2008-05-28 15:59:37 +0000

    This commit was manufactured by cvs2svn to create tag 'V02-10-01'.
    Sprout from master 2008-05-28 15:59:36 UTC muzaffar 'fixed the python _cf[fi].py rules so that it does not print error message when there are no cff or cfi python modules'
    Cherrypick from forBinLess_SCRAM 2008-05-08 16:29:34 UTC muzaffar 'python config updated for scram v1.2.1':
        CMSSW_BuildFile.xml
        CMSSW_SCRAM_ExtraBuildRule.pm
        CMSSW_Self.xml
        CMSSW_boot.xml
        CMSSW_bootsrc.xml
        CORAL_BuildFile.xml
        CORAL_SCRAM_ExtraBuildRule.pm
        CORAL_Self.xml
        CORAL_bootsrc.xml
        updateConfig.pl
    Delete:
        BuildFile
        BuildFile.xml
        CMSSW_BuildFile.file
        CMSSW_bootsrc
        CMSSW_requirements
        CMSSW_requirements.file
        CORAL_bootsrc
        CORAL_requirements
        Self
        Self.xml
        boot
        boot.xml
        bootsrc
        linkexternal.pl
        obviate_buildfiles.pl
        projectAreaRename.pl
        requirements
        requirements.xml
        scram_version
        site/sitename
        site/tools-FNAL.conf

---
+++ CORAL_SCRAM_ExtraBuildRule.pm
@@ -0,0 +1,62 @@
+package SCRAM_ExtraBuildRule;
+require 5.004;
+use Exporter;
+@ISA=qw(Exporter);
+
+sub new()
+{
+  my $class=shift;
+  my $self={};
+  $self->{template}=shift;
+  bless($self, $class);
+  return $self;  
+}
+
+sub isPublic ()
+   {
+   my $self=shift;
+   my $class = shift;
+   if ($class eq "LIBRARY") {return 1;}
+   elsif($class eq "SEAL_PLATFORM"){return 1;}
+   elsif($class eq "CLASSLIB"){return 1;}
+   return 0;
+   }
+
+sub Project ()
+{
+  my $self=shift;
+  my $common=$self->{template};
+  my $fh=$common->filehandle();
+  $common->addPluginSupport("seal","SEALPLUGIN","SealPluginRefresh",'\/plugins$',"SCRAMSTORENAME_LIB_MODULES",".cache",'$name="${name}.reg"',"");
+  $common->addProductDirMap("bin",'\/tests$',"SCRAMSTORENAME_TESTS_BIN",1);
+  $common->addProductDirMap("bin",'^src\/Tests\/.+',"SCRAMSTORENAME_TESTS_BIN",1);
+  $common->addProductDirMap("lib",'\/tests$',"SCRAMSTORENAME_TESTS_LIB",1);
+  $common->addProductDirMap("lib",'^src\/Tests\/.+',"SCRAMSTORENAME_TESTS_LIB",1);
+  $common->addPluginDirMap("seal",'\/tests$',"SCRAMSTORENAME_TESTS_LIB_MODULES");
+  print $fh "CONFIGDEPS += \$(WORKINGDIR)/cache/project_includes\n",
+            "\$(WORKINGDIR)/cache/project_includes: FORCE_TARGET\n",
+            "\t\@for f in \$(SCRAM_SOURCEDIR)/*; do        \\\n",
+            "\t  name=`basename \$\$f`;				\\\n",
+            "\t  f=\$\$f/\$\$name;					\\\n",
+            "\t  [ -d \$\$f ] || continue;                       \\\n",
+            "\t  if [ ! -e \$(LOCALTOP)/include/\$\$name ] ; then	\\\n",
+            "\t    ln -s ../\$\$f include/\$\$name ;	        \\\n",
+            "\t  fi; 						\\\n",
+            "\tdone\n",
+            "\t\@if [ ! -f \$@ ] ; then touch \$@; echo \$(\@F) DONE; fi\n\n";
+  return 1;
+}
+
+sub Extra_template()
+{
+  my $self=shift;
+  my $common=$self->{template};
+  $common->set("plugin_name",$common->core()->flags("SEAL_PLUGIN_NAME"));
+  $common->plugin_template();
+  $common->pushstash();
+  $common->dict_template();
+  $common->popstash();
+  return 1;
+}
+
+1;
