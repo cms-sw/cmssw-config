6a72b65653de11c23f9c9bce55f68043ed0a8921
smuzaffar <cmsbuild@cern.ch>
2011-05-16 11:54:16 +0000

    do not export lib and dependency if a library is declared as plugin

--- SCRAM/Plugins/BuildRules.pm
+++ SCRAM/Plugins/BuildRules.pm
@@ -1313,7 +1313,7 @@ sub runTemplate ()
 #############################################
 sub setLCGProjectLibPrefix ()
 {my $self=shift;$self->{cache}{LCGProjectLibPrefix}=shift;}
-sub safename_coral (){return &safename_CMSProjects(shift,"safename_SubsystemPackageBased",shift);}
+sub safename_coral (){&safename_LCGProjects(shift,shift,$self->{cache}{LCGProjectLibPrefix});}
 sub safename_LCGProjects ()
 {
   my $self=shift;
@@ -1845,7 +1845,7 @@ sub library_template_generic ()
   if ($localbf ne "")
   {
     my $ex=$core->data("EXPORT");
-    if(($core->publictype() == 1) && ($ex ne ""))
+    if(($core->publictype() == 1) && ($ex ne "") && ($self->get("plugin_type") eq ""))
     {
       foreach my $data ("INCLUDE","LIB")
       {
