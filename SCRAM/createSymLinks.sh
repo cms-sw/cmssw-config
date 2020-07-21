f85a7de2541356ef82224e313285970479d44adf
tulamor <romano@linuxmail.org>
2020-07-21 20:57:49 +0200

    regular expressions fixed in code

--- SCRAM/createSymLinks.sh
+++ SCRAM/createSymLinks.sh
@@ -21,33 +21,32 @@ if [ $# -gt 5 ]; then
     shift
   done
 fi
-srcfilter="$(echo "$srcfilter" | sed 's/\|$//')"
-srcnfilter="$(echo "$srcnfilter" | sed 's/\|$//')"
+srcfilter="$(echo "$srcfilter" | sed 's/|$//')"
+srcnfilter="$(echo "$srcnfilter" | sed 's/|$//')"
 
 function getSubDir () {
   local dir=$1
   local sdir=$2
-  if [ "X$sdir" = "X" ] ; then printf "\n" && return; fi
+  if [ "X$sdir" = "X" ] ; then return; fi
   if [ "X$sdir" = "X." ] ; then sdir=$"/`basename $dir`" ; else sdir="/${sdir}" ; fi
   printf "$sdir\n"
 }
 
 if [ -d "$src" ] ; then
-  for dir in "$(find $src -maxdepth $depth -mindepth $depth -name "*" -type d)"; do
+  for dir in $(find $src -maxdepth $depth -mindepth $depth -name "*" -type d); do
     if [[ $dir =~ ^\. ]]; then continue; fi
     if [ ! -z "$srcnfilter" ] && [[ dir =~ $srcnfilter ]]; then continue; fi
-    if [ ! -z "$srcfilter" ] && [[ dir =~ $srcfilter ]]; then continue; fi
+    if [ ! -z "$srcfilter" ] && [[ ! dir =~ $srcfilter ]]; then continue; fi
     rpath=$dir
     rpath="$(echo "$rpath" | sed "s|${src}/*||")"
     sdir=$(getSubDir "$dir" "$subdir")
     ldir=$(getSubDir "$dir" "$linkdir")
     slink="${des}/${rpath}${ldir}"
-    printf "$slink \n"
     if [ -d "${dir}${sdir}" ]; then
       if [ "$des" = python ] &&  [ -d $slink ]; then rm -rf $slink; fi
       slinkdir=$(dirname "$slink")
-      ldir="slinkdir"
-      ldir="$(echo "$ldir" | sed 's/[a-zA-Z0-9-_]+/../g')"
+      ldir="$slinkdir"
+      ldir="$(echo "$ldir" | sed -E 's/[a-zA-Z0-9_-]+/../g')"
       if [ ! -h "$slink" ]; then
         [ -d $slinkdir ] || mkdir -p $slinkdir; ln -s ${ldir}/${dir}${sdir} $slink
         printf "  ${dir}${sdir} -> $slink\n"
@@ -61,7 +60,7 @@ if [ -d "$des" ] ; then
  declare -A rm
  declare -A ok
  for d in $(find $des -name "*" -type l) ; do
-  local d1 = d
+  d1=$d
   d1="$(echo "d1" | sed 's/\/[^\/]+$//')"
   if [ ! -e  $d ]; then
     unlink $d
