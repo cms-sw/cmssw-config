d13e8a68e465743e9285d01949229fa96f035ca3
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-07-24 09:26:00 +0200

    more fixes for scramv3

--- SCRAM/scram_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-errfile="${SCRAM_INTwork}/build_error"
-rm -f ${errfile}
-(${SCRAM_GMAKE_PATH}gmake "$@" && [ ! -e ${errfile} ]) || (err=$?; echo "gmake: *** [There are compilation/build errors. Please see the detail log above.] Error $err" && exit $err)
