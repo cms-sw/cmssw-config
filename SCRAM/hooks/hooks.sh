37c325402eca53da6122413e8062c9575a048629
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-01-24 11:18:18 +0100

    Allow to ignore hooks via SCRAM_IGNORE_HOOKS file

--- SCRAM/hooks/hooks.sh
+++ SCRAM/hooks/hooks.sh
@@ -4,6 +4,10 @@ HOOK_TYPE=$(basename $0 | sed -e 's|-hook$||')
 export LC_ALL=C
 if [ -e ${SCRIPT_DIR}/${HOOK_TYPE} ] ; then
   for hook in $(find ${SCRIPT_DIR}/${HOOK_TYPE} -type f | sort) ; do
+    hook_name=$(echo $hook | sed "s|${SCRIPT_DIR}/||")
+    if [ "${SCRAM_IGNORE_HOOKS}" != "" -a -e "${SCRAM_IGNORE_HOOKS}" ] ;  then
+      grep -q "^${hook_name}$" "${SCRAM_IGNORE_HOOKS}" && continue
+    fi
     [ -x $hook ] && $hook
   done
 fi
