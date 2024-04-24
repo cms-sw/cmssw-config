f4041979faaa4bbbc3a50eb2467fa3718b15a033
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-04-24 12:22:26 +0200

    search for symlinked hooks

--- SCRAM/hooks/hooks.sh
+++ SCRAM/hooks/hooks.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(dirname $0)
 HOOK_TYPE=$(basename $0 | sed -e 's|-hook$||')
 export LC_ALL=C
 if [ -e ${SCRIPT_DIR}/${HOOK_TYPE} ] ; then
-  for hook in $(find ${SCRIPT_DIR}/${HOOK_TYPE} -type f | sort) ; do
+  for hook in $(find ${SCRIPT_DIR}/${HOOK_TYPE} -type f -o -type l | sort) ; do
     hook_name=$(echo $hook | sed "s|${SCRIPT_DIR}/||")
     if [ "${SCRAM_IGNORE_HOOKS}" != "" -a -e "${SCRAM_IGNORE_HOOKS}" ] ;  then
       grep -q "^${hook_name}$" "${SCRAM_IGNORE_HOOKS}" && continue
