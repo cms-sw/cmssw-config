057af048dc3e0f86ee3c3989965078a033ca9f8d
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-08-04 13:40:19 +0200

    bug fix for 'use name=1'

--- SCRAM/Plugins/BuildRules.py
+++ SCRAM/Plugins/BuildRules.py
@@ -1389,6 +1389,7 @@ $(COMMON_WORKINGDIR)/cache/project_links: FORCE_TARGET
     def alpaka_extra_flags(self, bend):
         self.set('use_private', 'alpaka-%s' % bend)
         extra_use = []
+        parent = self.get("parent")
         for f in ["USE_ALPAKA", "USE_ALPAKA_" + bend.upper()]:
             for u in [d for d in self.core.get_flag_value(f).split(" ") if d]:
                 if u=="1": u=parent
