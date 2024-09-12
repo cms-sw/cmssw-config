d002d1cf55c6c423f64950f28cf1953204a8393a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-09-12 17:05:25 +0200

    improve backend-setup.sh for setting tool flags

--- SCRAM/utils/backend-setup.sh
+++ SCRAM/utils/backend-setup.sh
@@ -9,50 +9,55 @@ if [ ! -e ${TOOL} ] ; then
   exit 1
 fi
 
+TOOL_FLAG="$(echo ${backend} | tr a-z A-Z)_FLAGS"
+FLAG_VALUES=()
 if [ "${CAPS}" = "reset" ] ; then
   cp -f $CMSSW_RELEASE_BASE/${TOOL} ${TOOL}.tmp
 elif [ "${backend}" = "cuda" ] ; then
   if [ "${CAPS}" = "native" ] ; then
-    DOTS=$(cudaComputeCapabilities | awk '{ print $2 }' | sort -u)
+    DOTS=$(cudaComputeCapabilities | awk '{ print "sm_"$2 }')
     CAPS=$(echo $DOTS | sed -e 's#\.*##g')
     if [ "${CAPS}" = "" ] ; then
       echo "Warning: Unable to find cuda compute capabilities." >&2
       exit 0
     fi
-  else
-    CAPS=$(echo ${CAPS} | tr ',' '\n' | sed 's|^sm_||' | tr '\n' ' ' | sed 's| *$||')
   fi
   cp -f ${TOOL} ${TOOL}.tmp
-  # remove existing capabilities
-  sed -i -e "s# *-gencode arch=compute_..,code=sm_.. *# #g" ${TOOL}.tmp
-  sed -i -e "s# *-gencode arch=compute_..,code=\[sm_..,compute_..\] *# #g" ${TOOL}.tmp
 
-  # add support for the capabilities found on this machine
-  for CAP in $CAPS; do
-    sed -i -e "/flags CUDA_FLAGS=/s#=\"#=\"-gencode arch=compute_$CAP,code=[sm_$CAP,compute_$CAP] #" ${TOOL}.tmp
+  # remove existing capabilities
+  sed -r -i -e 's/-gencode +arch=compute[^ "]+//g' ${TOOL}.tmp
+
+  # set supported capabilities found on this host
+  for CAP in $(echo $CAPS | tr ' ,' '\n\n' | sort -u | sed 's#^sm_##g'); do
+    FLAG_VALUES+=("-gencode arch=compute_$CAP,code=[sm_$CAP,compute_$CAP]")
   done
-  CAPS=$(echo $CAPS | tr ' ' '\n' | sed 's|^|sm_|' | tr '\n' ',' | sed 's|,$||')
 elif [ "${backend}" = "rocm" ] ; then
   if [ "${CAPS}" = "native" ] ; then
-    CAPS=$(rocmComputeCapabilities | awk '{ print $2 }' | sort -u)
+    CAPS=$(rocmComputeCapabilities | awk '{ print $2 }' | tr '\n' ' ')
     if [ "${CAPS}" = "" ] ; then
       echo "Warning: Unable to find rocm compute capabilities." >&2
       exit 0
     fi
-  else
-    CAPS=$(echo ${CAPS} | tr ',' ' ')
   fi
   cp -f ${TOOL} ${TOOL}.tmp
-  # remove existing capabilities flags
-  sed -r -i -e '/ROCM_FLAGS/s/--offload-arch=gfx[0-9a-f]+//g' ${TOOL}.tmp
-  # remove empty ROCM_FLAGS lines
-  sed -r -i -e '/ROCM_FLAGS=" *"/d' ${TOOL}.tmp
 
-  #add support for the capabilities found on this machine
-  for CAP in $CAPS; do
-    sed -i -e "s#</client>#</client>\n  <flags ROCM_FLAGS=\"--offload-arch=${CAP}\"/>#" ${TOOL}.tmp
+  # remove existing capabilities flags
+  sed -r -i -e 's/--offload-arch=gfx[0-9a-f]+//g' ${TOOL}.tmp
+
+  # set supported capabilities found on this host
+  for CAP in $(echo $CAPS | tr ' ,' '\n\n' | sort -u); do
+    FLAG_VALUES+=("--offload-arch=${CAP}")
   done
-  CAPS=$(echo $CAPS | tr ' ' ',' | sed 's|,$||')
+fi
+
+if [ ! -z "${FLAG_VALUES}" ]; then
+  # add the new flags
+  for ((i = 0; i < ${#FLAG_VALUES[@]}; i++)) ; do
+    sed -i -e "s#</client>#</client>\n  <flags ${TOOL_FLAG}=\"${FLAG_VALUES[$i]}\"/>#" ${TOOL}.tmp
+  done
+
+  # remove empty tool flags lines
+  sed -r -i -e "/ ${TOOL_FLAG}=\" *\"/d" ${TOOL}.tmp
 fi
 
 if [ $(diff -w ${TOOL}.tmp ${TOOL} | wc -l) -gt 0 ] ; then
@@ -64,6 +69,8 @@ if [ $(diff -w ${TOOL}.tmp ${TOOL} | wc -l) -gt 0 ] ; then
   else
     echo "set to ${CAPS}"
   fi
+  echo "${backend} tool has been updated. It is recommanded to re-build all packages which requires ${backend}."
+  echo "You can run \`scram build checkdeps\` once to checkout all packages which depend on ${backend}."
 else
   [ -f .SCRAM/${SCRAM_ARCH}/tools/${backend} ] && touch .SCRAM/${SCRAM_ARCH}/tools/${backend}
   rm -f ${TOOL}.tmp
