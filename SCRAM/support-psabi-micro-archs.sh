d63a869f87b2f35c6da111e9ff322ff5e61acd3b
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-04 11:51:49 +0100

    set SCRAM_MIN_SUPORTED_MICROARCH

--- SCRAM/support-psabi-micro-archs.sh
+++ SCRAM/support-psabi-micro-archs.sh
@@ -6,4 +6,4 @@ PSABI_ARCHS=$(ld.so --help | grep -E " ${PSABI_ARCH_PREFIX}[0-9]+ " | grep -i su
 MULTIARCH_TARGET=$(grep '"SCRAM_TARGET"' ${LOCALTOP}/config/Self.xml | grep ' value=' | sed 's|^ .*value="||;s|".*||')
 SCRAM_DEFAULT_MICROARCH=$(grep '"SCRAM_DEFAULT_MICROARCH"' ${LOCALTOP}/config/Self.xml | grep ' value=' | sed 's|^ .*value="||;s|".*||')
 SCRAM_TARGETS=$(grep 'SCRAM_TARGETS="' ${LOCALTOP}/config/Self.xml | sed 's|^.*SCRAM_TARGETS="||;s|".*||')
-SCRAM_ALL_MICROARCHS=$(echo ${SCRAM_DEFAULT_MICROARCH} ${SCRAM_TARGETS} | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
+SCRAM_ALL_MICROARCHS=$(echo ${SCRAM_DEFAULT_MICROARCH} ${SCRAM_TARGETS} | tr ' ' '\n' | grep "${PSABI_ARCH_PREFIX}" | sort | uniq | tr '\n' ' ')
