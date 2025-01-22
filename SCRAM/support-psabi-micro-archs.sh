eca5d85d7fa7a18910edfac712c71cf1a6680b1c
iarspider <iarspider@gmail.com>
2025-01-22 11:53:41 +0100

    Update support-psabi-micro-archs.sh
    There is no `ld.so` in SLC7. `ld --help` works for SLC7, EL8 and EL9.

--- SCRAM/support-psabi-micro-archs.sh
+++ SCRAM/support-psabi-micro-archs.sh
@@ -2,7 +2,7 @@
 [ $(uname -m) = "x86_64" ] || exit 0
 [ "${SCRAM}" != "" ] || SCRAM=scram
 PSABI_ARCH_PREFIX="x86-64-v"
-PSABI_ARCHS=$(ld.so --help | grep -E " ${PSABI_ARCH_PREFIX}[0-9]+ " | grep -i supported | sed 's|^ *||;s| .*||' | tr '\n' ' ')
+PSABI_ARCHS=$(ld --help | grep -E " ${PSABI_ARCH_PREFIX}[0-9]+ " | grep -i supported | sed 's|^ *||;s| .*||' | tr '\n' ' ')
 MULTIARCH_TARGET=$(grep '"SCRAM_TARGET"' ${LOCALTOP}/config/Self.xml | grep ' value=' | sed 's|^ .*value="||;s|".*||')
 SCRAM_DEFAULT_MICROARCH=$(grep '"SCRAM_DEFAULT_MICROARCH"' ${LOCALTOP}/config/Self.xml | grep ' value=' | sed 's|^ .*value="||;s|".*||')
 SCRAM_TARGETS=$(grep 'SCRAM_TARGETS="' ${LOCALTOP}/config/Self.xml | sed 's|^.*SCRAM_TARGETS="||;s|".*||')
